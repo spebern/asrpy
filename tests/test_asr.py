df3b14116cc0efe7b5bcf800371ea2d5dcd801c8
bold <bernhard@specht.net>
2024-03-22 22:48:04 +0100

    Replace np.int with int

--- tests/test_asr.py
+++ tests/test_asr.py
@@ -9,7 +9,6 @@ from scipy.io import loadmat
 from asrpy import ASR
 from mne.io import read_raw_eeglab
 from mne.datasets import testing
-from mne.utils import run_tests_if_main
 
 # set paths
 data_path = op.join(testing.data_path(download=True), 'EEGLAB')
@@ -20,7 +19,7 @@ valid_data_path = "./data/matlab_asr_data.mat"
 def test_asr():
     """Test whether ASR correlates sufficiently with original version."""
     valid_data = loadmat(valid_data_path)["data"][0][0][0]
-    raw = read_raw_eeglab(eeg_fname)
+    raw = read_raw_eeglab(eeg_fname, preload=True)
 
     # calculate clean data using ASR
     asr = ASR(sfreq=raw.info["sfreq"], cutoff=2.5, blocksize=10, win_len=0.5,
@@ -31,8 +30,9 @@ def test_asr():
                             maxdims=0.66)
 
     # check if the data is highly equal to the MATLAB data
-    corrs = [np.corrcoef(i, j)[0, 1] for (i, j) in zip(cleaned, valid_data)]
+    corrs = [np.corrcoef(i, j)[0, 1] for (i, j) in zip(cleaned.get_data(), valid_data)]
+    print(np.mean(corrs))
     assert np.mean(corrs) > 0.94
 
 
-run_tests_if_main()
+test_asr()
